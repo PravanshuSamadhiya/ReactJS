4de2cbd5cd7a68af81be9e4b2ffe15595ec8a8a5
Pravanshu Samadhiya <142601614+PravanshuSamadhiya@users.noreply.github.com>
2024-03-16 11:22:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import './App.css';
-// import { useState ,useEffect} from 'react';
-// import Axios from 'axios';
-import Data from "./Data"
+import "./App.css";
 
 function App() {
-  // const[catFact, setCatFact] = useState("");
-
-  // const FetchCatData = () => {
-  //    Axios.get("https://catfact.ninja/fact").then((res) => {
-  //       setCatFact(res.data.fact);
-  //    });
-  // }
-
-  // useEffect(() =>{
-  //   FetchCatData();
-  // } ,[])
-
-    return (
-
-        <div className='App'>
-          <Data/>
-            {/* <button onClick={FetchCatData}>Generate Cat Facts</button>
-            <p>{catFact}</p> */}
-          
-        </div>
-    );
+  return <div className="App"> Hello World </div>;
 }
+
 export default App;
